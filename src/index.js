3ff425ba67db2f1d9f36ec78d438c3e59d8a8e7b
VladislavVV1 <vlad1905vvv@gmail.com>
2023-11-10 17:37:25 -0700

    lesson 11

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import { App } from "components/App";
+import "./index.css";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
